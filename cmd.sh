66b2a072ed45573c65d728e6bad4b246282a507b
chuong98 <chuong98vt@gmail.com>
2022-08-28 23:27:06 -0700

    ok

--- cmd.sh
+++ cmd.sh
@@ -4,5 +4,6 @@ python trdg/run.py --output_dir='out' \
     -rk -k 5 --name_format 2 \
     -obb 2 --distorsion 3 \
     --text_color '#000000,#888888' \
-    --font_dir=fonts --margins 10,10,10,10 
-    # -rs -let -num \
+    --font_dir=fonts --margins 10,10,10,10 \
+    -rs -let -num -sym 
+    # -dt "a,b,c,1,2,3"
