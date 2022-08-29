60a37422fc4c486c906f9ab91687caa4e2229eec
chuong98 <chuong98vt@gmail.com>
2022-08-28 23:38:29 -0700

    remove broken fonts

--- cmd.sh
+++ cmd.sh
@@ -1,9 +1,9 @@
 python trdg/run.py --output_dir='out' \
-    --count 8 -w 3 -r \
+    --count 8 -w 10 -r \
     --format 64 --width 512 \
     -rk -k 5 --name_format 2 \
     -obb 2 --distorsion 3 \
     --text_color '#000000,#888888' \
     --font_dir=fonts --margins 10,10,10,10 \
-    -rs -let -num -sym 
-    # -dt "a,b,c,1,2,3"
+    --space_width 3
+    # -rs -let -num -sym 
